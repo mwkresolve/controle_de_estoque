b3a68fb0a9d769c25975ae6071488e475c5e4d4f
mwkresolve <mwkresolve@gmail.com>
2023-06-06 07:58:46 -0300

    algumas correcoes

--- estoque/views.py
+++ estoque/views.py
@@ -19,18 +19,15 @@ def cadastrar_produto(request):
             return redirect('cadastrar_produto')
     else:
         form = ProdutoForm()
-
     return render(request, 'cadastrar_produto.html', {'form': form})
 def produtos_cadastrados(request):
     sort = request.GET.get('sort', 'nome')  # Obtém o parâmetro de ordenação da query string, padrão é "nome"
     produtos = Produto.objects.order_by(sort)  # Ordena os produtos de acordo com o campo selecionado
-
     # Configuração da paginação
     paginator = Paginator(produtos, 20)  # Exibe 20 produtos por página
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-
-    context = {'page_obj': page_obj}
+    context = {'page_obj': page_obj, 'produtos':produtos}
     return render(request, 'produtos_cadastrados.html', context)
 def produto_delete(request, pk):
     produto = Produto.objects.get(pk=pk)
